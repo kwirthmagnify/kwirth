84312b98848e00bd424e9ba438aac55fffe94efd
Julio Fernandez <jfvilas@outlook.com>
2026-04-17 18:32:34 +0200

    create a docker tool for image-committer

---
+++ tools/nerdctl/nerdctl-build.cmd
@@ -0,0 +1,4 @@
+git clone https://github.com/containerd/nerdctl.git
+cd nerdctl
+docker build -t kwirthmagnify/nerdctl .
+docker push kwirthmagnify/nerdctl
---
+++ tools/nerdctl/nerdctl-clone.cmd
@@ -0,0 +1,3 @@
+docker pull ghcr.io/containerd/nerdctl:latest
+docker tag ghcr.io/containerd/nerdctl:latest kwirthmagnify/nerdctl
+docker push kwirthmagnify/nerdctl
