bf511d05eb5ff88970c9ad62d8bab1b3f8856823
Aisdk Vercel <aisdkvercel@outlook.com>
2026-05-04 17:37:34 +0200

    update version and presentation to 0.5.187

--- electron/version.js
+++ electron/version.js
@@ -1,1 +1,1 @@
-const VERSION="0.5.84"; module.exports = { VERSION }; 
+const VERSION="0.5.87"; module.exports = { VERSION }; 
--- front/src/version.ts
+++ front/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION:string="0.5.84"; 
+export const VERSION:string="0.5.87"; 
