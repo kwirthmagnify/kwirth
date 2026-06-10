181a95373c6e3e3aa39d245944026eac64775063
Aisdk Vercel <aisdkvercel@outlook.com>
2026-06-10 21:29:12 +0200

    censor: bump with minor corrections

--- front/src/version.ts
+++ front/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION:string="0.5.151"; 
+export const VERSION:string="0.5.153"; 
--- version/version.js
+++ version/version.js
@@ -1,1 +1,1 @@
-const VERSION="0.5.151"; module.exports = { VERSION }; 
+const VERSION="0.5.153"; module.exports = { VERSION }; 
