2c4513cb71d2299c380a7aba6a3fdca4ace7fc05
Aisdk Vercel <aisdkvercel@outlook.com>
2026-06-17 19:39:24 +0200

    bump common-back dependents

--- back/src/version.ts
+++ back/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION:string="0.5.158"; 
+export const VERSION:string="0.5.159"; 
--- electron/version.js
+++ electron/version.js
@@ -1,1 +1,1 @@
-const VERSION="0.5.158"; module.exports = { VERSION }; 
+const VERSION="0.5.159"; module.exports = { VERSION }; 
