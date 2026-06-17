79dce799cf64d8de73c2c34347c44d1853c1a54a
Aisdk Vercel <aisdkvercel@outlook.com>
2026-06-17 10:46:01 +0200

    prepare providers for running on SEA

--- back/src/tools/ProviderManager.ts
+++ back/src/tools/ProviderManager.ts
@@ -113,7 +113,8 @@ export class ProviderManager {
         try {
             const resolved = require.resolve(backPath)
             if (require.cache[resolved]) delete require.cache[resolved]
-            const providerModule = require(backPath)
+            //const providerModule = require(backPath)
+            const providerModule = require('module').createRequire(__filename)(backPath)
             const ProviderClass = providerModule.default ?? Object.values(providerModule).find(v => typeof v === 'function')
             if (ProviderClass) {
                 registeredProviders.set(id, ProviderClass as TProviderConstructor)
@@ -302,7 +303,8 @@ export class ProviderManager {
         fs.writeFileSync(tmpPath, backJs)
         try {
             if (require.cache[require.resolve(tmpPath)]) delete require.cache[require.resolve(tmpPath)]
-            const providerModule = require(tmpPath)
+            //const providerModule = require(tmpPath)
+            const providerModule = require('module').createRequire(__filename)(tmpPath)
             const ProviderClass = providerModule.default ?? Object.values(providerModule).find(v => typeof v === 'function')
             if (ProviderClass) {
                 registeredProviders.set(id, ProviderClass as TProviderConstructor)
