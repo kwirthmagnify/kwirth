8da8eadc126ac95beb7a0506385b0f379f70dba9
Aisdk Vercel <aisdkvercel@outlook.com>
2026-08-31 13:12:17 +0200

    new version

--- back/src/version.ts
+++ back/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION:string="0.5.199"; 
+export const VERSION:string="0.5.201"; 
--- electron/version.js
+++ electron/version.js
@@ -1,1 +1,1 @@
-const VERSION="0.5.199"; module.exports = { VERSION }; 
+const VERSION="0.5.201"; module.exports = { VERSION }; 
