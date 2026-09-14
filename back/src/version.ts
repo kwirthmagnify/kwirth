d9cdc931cfc62ed6664db3efe099589357da0b83
Aisdk Vercel <aisdkvercel@outlook.com>
2026-09-14 08:27:04 +0200

    chore(version): 0.5.213

--- back/src/version.ts
+++ back/src/version.ts
@@ -1,1 +1,1 @@
-export const VERSION:string="0.5.212"; 
+export const VERSION:string="0.5.213"; 
--- electron/version.js
+++ electron/version.js
@@ -1,1 +1,1 @@
-const VERSION="0.5.212"; module.exports = { VERSION }; 
+const VERSION="0.5.213"; module.exports = { VERSION }; 
